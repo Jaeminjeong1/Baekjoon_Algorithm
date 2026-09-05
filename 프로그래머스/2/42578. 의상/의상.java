959a7e98a62d71325aa2c9e5e795e6fca16924dd
Jeong Jaemin <82368069+Jaeminjeong1@users.noreply.github.com>
2026-09-05 12:53:25 +0900

    [level 2] Title: 의상, Time: 0.21 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        Map<String, Integer> map = new HashMap<>();
         
-        for (String[] c : clothes){
-            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
+        HashMap<String, Integer> map = new HashMap();
+        
+        for(String[] clothe : clothes){
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
         }
         
-        for(int a : map.values()){
-            answer *= (a + 1);
+        int answer = 1;
+        for(int value : map.values()){
+            answer *= (value + 1);
         }
         
         return answer - 1;
